c330afb8b1f41bac8c807a187c94cac2b6489d6a
jbeck <j.beck@ub.uni-frankfurt.de>
2017-08-22 17:26:59 +0200

    Add basic fiddk theme structure.

---
+++ themes/fiddk/theme.config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'extends' => 'bootstrap3',
+    'css' => array(
+      //'fiddk.css:screen, projection',
+    ),
+    'less' => array(
+        'active' => false,
+        'compiled.less'
+    ),
+    'favicon' => 'fid-dk_favicon.png',
+);
