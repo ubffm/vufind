3f6f86c2bc2aa7b5afc576e7216bea14768d4b99
j4lib <j.beck@ub.uni-frankfurt.de>
2019-01-18 17:37:06 +0100

    New feedback forms.

--- module/Fiddk/config/module.config.php
+++ module/Fiddk/config/module.config.php
@@ -2,27 +2,118 @@
 namespace Fiddk\Module\Configuration;
 
 $config = [
+  'router' => [
+      'routes' => [
+          'dataprovider-page' => [
+              'type'    => 'Zend\Router\Http\Segment',
+              'options' => [
+                  'route'    => '/DataProvider/[:page]',
+                  'constraints' => [
+                      'page'     => '[a-zA-Z][a-zA-Z_-]*',
+                  ],
+                  'defaults' => [
+                      'controller' => 'DataProvider',
+                      'action'     => 'DataProvider',
+                  ]
+              ],
+          ],
+      ],
+  ],
   'controllers' => [
     'factories' => [
       'Fiddk\Controller\ContentController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\DproviderController' => 'VuFind\Controller\AbstractBaseFactory',
-      //'ajax' => 'fiddk\Controller\Factory::getAjaxController',
+      'Fiddk\Controller\DataProviderController' => 'VuFind\Controller\AbstractBaseFactory',
+      'Fiddk\Controller\AuthorityController' => 'VuFind\Controller\AbstractBaseFactory',
+      'Fiddk\Controller\RecordController' => 'VuFind\Controller\AbstractBaseWithConfigFactory',
+      'Fiddk\Controller\SearchController' => 'VuFind\Controller\AbstractBaseWithConfigFactory'
       //'record' => 'fiddk\Controller\Factory::getRecordController',
     ],
     'aliases' => [
+      'DataProvider' => 'Fiddk\Controller\DataProviderController',
+      'dataprovider' => 'Fiddk\Controller\DataProviderController',
+
       // Overrides
       'VuFind\Controller\ContentController' => 'Fiddk\Controller\ContentController',
-      //'VuFind\Controller\SearchController' => 'fiddk\Controller\SearchController'
+      'VuFind\Controller\RecordController' => 'Fiddk\Controller\RecordController',
+      'VuFind\Controller\AuthorityController' => 'Fiddk\Controller\AuthorityController',
+      'VuFind\Controller\SearchController' => 'Fiddk\Controller\SearchController',
+
+      //'record' => 'Fiddk\Controller\RecordController',
     ]
   ],
   'vufind' => [
-      'recorddriver_tabs' => [
-        'Fiddk\RecordDriver\SolrEdm' => [
-          'tabs' => [
-            'Description' => null,
-            'Similar' => 'SimilarItemsCarousel',
-            'Details' => 'StaffViewArray',
-          ]
+    'plugin_managers' => [
+      'ajaxhandler' => [
+        'factories' => [
+          'Fiddk\AjaxHandler\GetTotalResults' =>
+              'Fiddk\AjaxHandler\GetTotalResultsFactory',
+                ],
+        'aliases' => [
+          'GetTotalResults' => 'Fiddk\AjaxHandler\GetTotalResults',
+        ],
+      ],
+      'recommend' => [
+        'factories' => [
+          'Fiddk\Recommend\AuthorityInfo' => 'Fiddk\Recommend\Factory::getAuthorityInfo',
+        ],
+        'aliases' => [
+          'authorityinfo' => 'Fiddk\Recommend\AuthorityInfo',
+        ],
+      ],
+      'search_options' => [
+        'factories' => [
+          'Fiddk\Search\SolrAuthorityInfo\Options' => 'VuFind\Search\Options\OptionsFactory',
+          ],
+        'aliases' => [
+          'solrauthorityinfo' => 'Fiddk\Search\SolrAuthorityInfo\Options',
+          ],
+      ],
+      'search_params' => [
+        'factories' => [
+          'Fiddk\Search\SolrAuthorityInfo\Params' => 'VuFind\Search\Solr\ParamsFactory',
+          ],
+        'aliases' => [
+          'solrauthorityinfo' => 'Fiddk\Search\SolrAuthorityInfo\Params',
+          ],
+      ],
+      'search_results' => [
+        'factories' => [
+          'Fiddk\Search\SolrAuthorityInfo\Results' => 'VuFind\Search\Solr\ResultsFactory',
+          ],
+        'aliases' => [
+          'solrauthorityinfo' => 'Fiddk\Search\SolrAuthorityInfo\Results',
+          ],
+      ],
+      'recorddriver' => [
+        'factories' => [
+          'Fiddk\RecordDriver\SolrEdm' =>
+              'VuFind\RecordDriver\SolrDefaultFactory',
+          'Fiddk\RecordDriver\SolrAuthEdm' =>
+              'VuFind\RecordDriver\SolrDefaultWithoutSearchServiceFactory',
+        ],
+        'aliases' => [
+          'solredm' => 'Fiddk\RecordDriver\SolrEdm',
+          'solrauthedm' => 'Fiddk\RecordDriver\SolrAuthEdm',
+          'solrauth' => 'Fiddk\RecordDriver\SolrAuthEdm',
+        ],
+      ],
+      'recordtab' => [
+        'factories' => [
+          'Fiddk\RecordTab\StaffViewEdm' => 'Zend\ServiceManager\Factory\InvokableFactory',
+        ],
+        'aliases' => [
+          'staffviewedm' => 'Fiddk\RecordTab\StaffViewEdm',
+        ],
+      ],
+    ],
+    'recorddriver_tabs' => [
+      'Fiddk\RecordDriver\SolrEdm' => [
+        'tabs' => [
+          'TOC' => 'TOC',
+          'Similar' => 'SimilarItemsCarousel',
+          'Details' => 'StaffViewEdm',
+        ],
+        'defaultTab' => null,
         ],
       ],
     ],
@@ -34,10 +125,6 @@ $recordRoutes = [
     'collection' => 'Collection',
     'missingrecord' => 'MissingRecord',
     'solrauthrecord' => 'Authority'
-];
-
-$staticRoutes = [
-        'Search/AuthorityResults'
 ];
 
 $routeGenerator = new \VuFind\Route\RouteGenerator();
