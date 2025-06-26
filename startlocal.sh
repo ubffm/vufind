4fb340b6a41930e2b2c836686135127decbd6db4
jsmendek <j.smendek@ub.uni-frankfurt.de>
2025-06-26 08:40:28 +0200

    Change: delete cache, when start local server

--- startlocal.sh
+++ startlocal.sh
@@ -4,6 +4,7 @@ export VUFIND_APPLICATION_PATH=$(pwd)
 export VUFIND_LOCAL_DIR=$(pwd)/local
 export VUFIND_LOCAL_MODULES=Fiddk,FiddkApi
 export VUFIND_CACHE_DIR=$(pwd)/local/cache
+rm -rf "${VUFIND_CACHE_DIR:?}"/*
 php -S localhost:8000 -t public
 
 
