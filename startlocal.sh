aecc4560e00683c71dac790335fb1f0541f3a9d1
jsmendek <j.smendek@ub.uni-frankfurt.de>
2025-12-11 13:22:37 +0100

    Change startlocal

--- startlocal.sh
+++ startlocal.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/bash
 export VUFIND_HOME=$(pwd)
+echo $VUFIND_HOME
 export VUFIND_APPLICATION_PATH=$(pwd)
 export VUFIND_LOCAL_DIR=$(pwd)/local
 export VUFIND_LOCAL_MODULES=Fiddk,FiddkApi
 export VUFIND_CACHE_DIR=$(pwd)/local/cache
 rm -rf "${VUFIND_CACHE_DIR:?}"/*
+if nc -z localhost 8983; then
+    echo "Solr läuft bereits."
+else
+    echo "Solr läuft nicht – starte Solr..."
+    sh solr.sh start
+fi
 php -S localhost:8000 -t public
 
 
