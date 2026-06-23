48e65dc74ad0e2b27d73d2b84e8054a3dd66ff29
jsmendek <j.smendek@ub.uni-frankfurt.de>
2026-06-23 13:34:56 +0200

    style: Korrigiere Einrückung und Leerzeichen in module.config.php

--- module/Fiddk/config/module.config.php
+++ module/Fiddk/config/module.config.php
@@ -1,156 +1,157 @@
 <?php
+
 // namespace Fiddk\Module\Config;
 
 $config = [
     'view_helpers' => [
-      'factories' => [
-        \Fiddk\View\Helper\RecordExists::class => \Fiddk\View\Helper\RecordExistsFactory::class,
-      ],
-      'aliases' => [
-        'recordExists' => \Fiddk\View\Helper\RecordExists::class,
-      ],
+        'factories' => [
+            \Fiddk\View\Helper\RecordExists::class => \Fiddk\View\Helper\RecordExistsFactory::class,
+        ],
+        'aliases' => [
+            'recordExists' => \Fiddk\View\Helper\RecordExists::class,
+        ],
     ],
 
-  'router' => [
-      'routes' => [
-          'dataprovider-page' => [
-              'type'    => 'Laminas\Router\Http\Segment',
-              'options' => [
-                  'route'    => '/DataProvider/[:page]',
-                  'constraints' => [
-                      'page'     => '[a-zA-Z][a-zA-Z_-]*',
-                  ],
-                  'defaults' => [
-                      'controller' => 'DataProvider',
-                      'action'     => 'DataProvider',
-                  ]
-              ],
-          ],
-          'showcase' => [
-            'type' => 'Laminas\Router\Http\Literal',
-            'options' => [
-              'route'    => '/Showcase',
-            'defaults' => [
-              'controller' => 'Showcase',
-              'action'     => 'Home',
-            ]
-            ]
-          ],
-      ],
-  ],
-  'controllers' => [
-    'factories' => [
-      'Fiddk\Controller\ContentController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\DataProviderController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\AgentController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\CorporationSearchController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\PersonSearchController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\EventSearchController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\EventController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\WorkSearchController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\WorkController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\SearchController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\FeedbackController' => 'VuFind\Controller\AbstractBaseFactory',
-      'Fiddk\Controller\ShowcaseController' => 'VuFind\Controller\AbstractBaseFactory',
+    'router' => [
+        'routes' => [
+            'dataprovider-page' => [
+                'type'    => 'Laminas\Router\Http\Segment',
+                'options' => [
+                    'route'    => '/DataProvider/[:page]',
+                    'constraints' => [
+                        'page'     => '[a-zA-Z][a-zA-Z_-]*',
+                    ],
+                    'defaults' => [
+                        'controller' => 'DataProvider',
+                        'action'     => 'DataProvider',
+                    ],
+                ],
+            ],
+            'showcase' => [
+                'type' => 'Laminas\Router\Http\Literal',
+                'options' => [
+                    'route'    => '/Showcase',
+                    'defaults' => [
+                        'controller' => 'Showcase',
+                        'action'     => 'Home',
+                    ],
+                ],
+            ],
+        ],
     ],
-    'aliases' => [
-      'DataProvider' => 'Fiddk\Controller\DataProviderController',
-      'dataprovider' => 'Fiddk\Controller\DataProviderController',
-      'Showcase' => 'Fiddk\Controller\ShowcaseController',
-      'showcase' => 'Fiddk\Controller\ShowcaseController',
-      'Agent' => 'Fiddk\Controller\AgentController',
-      'agent' => 'Fiddk\Controller\AgentController',
-      'CorporationSearch' => 'Fiddk\Controller\CorporationSearchController',
-      'corporationsearch' => 'Fiddk\Controller\CorporationSearchController',
-      'EventSearch' => 'Fiddk\Controller\EventSearchController',
-      'eventsearch' => 'Fiddk\Controller\EventSearchController',
-      'Event' => 'Fiddk\Controller\EventController',
-      'event' => 'Fiddk\Controller\EventController',
-      'PersonSearch' => 'Fiddk\Controller\PersonSearchController',
-      'personsearch' => 'Fiddk\Controller\PersonSearchController',
-      'Search' => 'Fiddk\Controller\SearchController',
-      'search' => 'Fiddk\Controller\SearchController',
-      'WorkSearch' => 'Fiddk\Controller\WorkSearchController',
-      'worksearch' => 'Fiddk\Controller\WorkSearchController',
-      'Work' => 'Fiddk\Controller\WorkController',
-      'work' => 'Fiddk\Controller\WorkController',
-      'Feedback' => 'Fiddk\Controller\FeedbackController',
-      'feedback' => 'Fiddk\Controller\FeedbackController',
+    'controllers' => [
+        'factories' => [
+            'Fiddk\Controller\ContentController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\DataProviderController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\AgentController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\CorporationSearchController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\PersonSearchController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\EventSearchController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\EventController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\WorkSearchController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\WorkController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\SearchController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\FeedbackController' => 'VuFind\Controller\AbstractBaseFactory',
+            'Fiddk\Controller\ShowcaseController' => 'VuFind\Controller\AbstractBaseFactory',
+        ],
+        'aliases' => [
+            'DataProvider' => 'Fiddk\Controller\DataProviderController',
+            'dataprovider' => 'Fiddk\Controller\DataProviderController',
+            'Showcase' => 'Fiddk\Controller\ShowcaseController',
+            'showcase' => 'Fiddk\Controller\ShowcaseController',
+            'Agent' => 'Fiddk\Controller\AgentController',
+            'agent' => 'Fiddk\Controller\AgentController',
+            'CorporationSearch' => 'Fiddk\Controller\CorporationSearchController',
+            'corporationsearch' => 'Fiddk\Controller\CorporationSearchController',
+            'EventSearch' => 'Fiddk\Controller\EventSearchController',
+            'eventsearch' => 'Fiddk\Controller\EventSearchController',
+            'Event' => 'Fiddk\Controller\EventController',
+            'event' => 'Fiddk\Controller\EventController',
+            'PersonSearch' => 'Fiddk\Controller\PersonSearchController',
+            'personsearch' => 'Fiddk\Controller\PersonSearchController',
+            'Search' => 'Fiddk\Controller\SearchController',
+            'search' => 'Fiddk\Controller\SearchController',
+            'WorkSearch' => 'Fiddk\Controller\WorkSearchController',
+            'worksearch' => 'Fiddk\Controller\WorkSearchController',
+            'Work' => 'Fiddk\Controller\WorkController',
+            'work' => 'Fiddk\Controller\WorkController',
+            'Feedback' => 'Fiddk\Controller\FeedbackController',
+            'feedback' => 'Fiddk\Controller\FeedbackController',
 
-      // Overrides
-      'VuFind\Controller\ContentController' => 'Fiddk\Controller\ContentController',
-      'VuFind\Controller\SearchController' => 'Fiddk\Controller\SearchController',
-      'VuFind\Controller\FeedbackController' => 'Fiddk\Controller\FeedbackController',
-    ]
-  ],
-  'service_manager' => [
-    'allow_override' => true,
-    'factories' => [
-      //'Fiddk\ContentBlock\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
-      'Fiddk\RecordDriver\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
-      'Fiddk\Search\Options\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
-      'Fiddk\Search\Params\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
-      'Fiddk\Search\Results\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
+            // Overrides
+            'VuFind\Controller\ContentController' => 'Fiddk\Controller\ContentController',
+            'VuFind\Controller\SearchController' => 'Fiddk\Controller\SearchController',
+            'VuFind\Controller\FeedbackController' => 'Fiddk\Controller\FeedbackController',
+        ],
     ],
-    // Overrides
-    'aliases' => [
-      //'VuFind\ContentBlock\PluginManager' => 'Fiddk\ContentBlock\PluginManager',
-      'VuFind\RecordDriver\PluginManager' => 'Fiddk\RecordDriver\PluginManager',
-      'VuFind\Search\Options\PluginManager' => 'Fiddk\Search\Options\PluginManager',
-      'VuFind\Search\Params\PluginManager' => 'Fiddk\Search\Params\PluginManager',
-      'VuFind\Search\Results\PluginManager' => 'Fiddk\Search\Results\PluginManager',
-    ],
-  ],
-  'vufind' => [
-    'plugin_managers' => [
-      'autocomplete' => [
+    'service_manager' => [
+        'allow_override' => true,
         'factories' => [
-          'Fiddk\Autocomplete\SolrPerson' => 'VuFind\Autocomplete\SolrFactory',
-          'Fiddk\Autocomplete\SolrCorporation' => 'VuFind\Autocomplete\SolrFactory',
-          'Fiddk\Autocomplete\SolrEvent' => 'VuFind\Autocomplete\SolrFactory',
-          'Fiddk\Autocomplete\SolrWork' => 'VuFind\Autocomplete\SolrFactory',
+            //'Fiddk\ContentBlock\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
+            'Fiddk\RecordDriver\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
+            'Fiddk\Search\Options\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
+            'Fiddk\Search\Params\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
+            'Fiddk\Search\Results\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
         ],
+        // Overrides
         'aliases' => [
-          'solrperson' => 'Fiddk\Autocomplete\SolrPerson',
-          'solrcorporation' => 'Fiddk\Autocomplete\SolrCorporation',
-          'solrevent' => 'Fiddk\Autocomplete\SolrEvent',
-          'solrwork' => 'Fiddk\Autocomplete\SolrWork',
+            //'VuFind\ContentBlock\PluginManager' => 'Fiddk\ContentBlock\PluginManager',
+            'VuFind\RecordDriver\PluginManager' => 'Fiddk\RecordDriver\PluginManager',
+            'VuFind\Search\Options\PluginManager' => 'Fiddk\Search\Options\PluginManager',
+            'VuFind\Search\Params\PluginManager' => 'Fiddk\Search\Params\PluginManager',
+            'VuFind\Search\Results\PluginManager' => 'Fiddk\Search\Results\PluginManager',
         ],
-      ],
-      'recommend' => [
-        'factories' => [
-          'Fiddk\Recommend\AuthorityInfo' => 'VuFind\Recommend\AuthorInfoFactory',
-        ],
-        'aliases' => [
-          'authorityinfo' => 'Fiddk\Recommend\AuthorityInfo',
-        ],
-      ],
-      'search_options' => [ /* See Fiddk\Search\Options\PluginManager for defaults */ ],
-      'search_params' => [ /* See Fiddk\Search\Params\PluginManager for defaults */ ],
-      'search_results' => [ /* See Fiddk\Search\Results\PluginManager for defaults */ ],
-      'search_backend' => [
-        'factories' => [ 
-          'SolrPerson' => 'Fiddk\Search\Factory\SolrPersonBackendFactory',
-          'SolrCorporation' => 'Fiddk\Search\Factory\SolrCorporationBackendFactory',
-          'SolrEvent' => 'Fiddk\Search\Factory\SolrEventBackendFactory',
-          'SolrWork' => 'Fiddk\Search\Factory\SolrWorkBackendFactory',
-        ],
-      ],
-      'recordtab' => [
-        'aliases' => [
-          'staffviewedm' => 'Fiddk\RecordTab\StaffViewEdm',
-        ],
-      ],
-      'contentblock' => [
-          'factories' => [
-              'statistics' => \Fiddk\ContentBlock\StatisticsFactory::class,
-          ],
-          'aliases' => [
-              'Statistics' => 'statistics',
-          ],
-      ],
     ],
-  ],
+    'vufind' => [
+        'plugin_managers' => [
+            'autocomplete' => [
+                'factories' => [
+                    'Fiddk\Autocomplete\SolrPerson' => 'VuFind\Autocomplete\SolrFactory',
+                    'Fiddk\Autocomplete\SolrCorporation' => 'VuFind\Autocomplete\SolrFactory',
+                    'Fiddk\Autocomplete\SolrEvent' => 'VuFind\Autocomplete\SolrFactory',
+                    'Fiddk\Autocomplete\SolrWork' => 'VuFind\Autocomplete\SolrFactory',
+                ],
+                'aliases' => [
+                    'solrperson' => 'Fiddk\Autocomplete\SolrPerson',
+                    'solrcorporation' => 'Fiddk\Autocomplete\SolrCorporation',
+                    'solrevent' => 'Fiddk\Autocomplete\SolrEvent',
+                    'solrwork' => 'Fiddk\Autocomplete\SolrWork',
+                ],
+            ],
+            'recommend' => [
+                'factories' => [
+                    'Fiddk\Recommend\AuthorityInfo' => 'VuFind\Recommend\AuthorInfoFactory',
+                ],
+                'aliases' => [
+                    'authorityinfo' => 'Fiddk\Recommend\AuthorityInfo',
+                ],
+            ],
+            'search_options' => [ /* See Fiddk\Search\Options\PluginManager for defaults */ ],
+            'search_params' => [ /* See Fiddk\Search\Params\PluginManager for defaults */ ],
+            'search_results' => [ /* See Fiddk\Search\Results\PluginManager for defaults */ ],
+            'search_backend' => [
+                'factories' => [
+                    'SolrPerson' => 'Fiddk\Search\Factory\SolrPersonBackendFactory',
+                    'SolrCorporation' => 'Fiddk\Search\Factory\SolrCorporationBackendFactory',
+                    'SolrEvent' => 'Fiddk\Search\Factory\SolrEventBackendFactory',
+                    'SolrWork' => 'Fiddk\Search\Factory\SolrWorkBackendFactory',
+                ],
+            ],
+            'recordtab' => [
+                'aliases' => [
+                    'staffviewedm' => 'Fiddk\RecordTab\StaffViewEdm',
+                ],
+            ],
+            'contentblock' => [
+                'factories' => [
+                    'statistics' => \Fiddk\ContentBlock\StatisticsFactory::class,
+                ],
+                'aliases' => [
+                    'Statistics' => 'statistics',
+                ],
+            ],
+        ],
+    ],
 ];
 
 // Define record view routes -- route name => controller
@@ -168,12 +169,12 @@ $recordRoutes = [
     'solrworkrecord' => 'work',
 ];
 
-$staticRoutes = ['EventSearch/Home','EventSearch/Results',
-                 'Event/FacetList','CorporationSearch/Results',
+$staticRoutes = ['EventSearch/Home', 'EventSearch/Results',
+                 'Event/FacetList', 'CorporationSearch/Results',
                  'Agent/Home',
-                 'PersonSearch/Results','Agent/FacetList',
-                 'WorkSearch/Home','WorkSearch/Results',
-                 'Work/FacetList','Search/More',
+                 'PersonSearch/Results', 'Agent/FacetList',
+                 'WorkSearch/Home', 'WorkSearch/Results',
+                 'Work/FacetList', 'Search/More',
                  'showcase/playbills'];
 
 $routeGenerator = new \VuFind\Route\RouteGenerator();
@@ -188,8 +189,8 @@ $config['router']['routes']['home'] = [
         'defaults' => [
             'controller' => 'Search',
             'action'     => 'Home',
-        ]
-    ]
+        ],
+    ],
 ];
 
 return $config;
